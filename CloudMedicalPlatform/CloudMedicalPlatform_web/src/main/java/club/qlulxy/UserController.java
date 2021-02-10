9d3291bbfec505bd85138b1f2f406e6471341b94
LeeXingyun <345581887@qq.com>
2021-02-10 20:29:50 +0800

    并添加到本地仓库

---
+++ CloudMedicalPlatform/CloudMedicalPlatform_web/src/main/java/club/qlulxy/UserController.java
@@ -0,0 +1,15 @@
+package club.qlulxy;
+
+/**
+ * @author ：李兴运
+ * @date ：Created in 2021/2/10 20:17
+ * @description：这是一个user的视图层实例
+ * @modified By：
+ * @version: 1.0
+ */
+public class UserController {
+    public static void main(String[] args) {
+        System.out.println("hello,world");
+        int a = 10;
+    }
+}
